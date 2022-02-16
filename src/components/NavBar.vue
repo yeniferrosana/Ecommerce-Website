0d1fc49a53cb8854a5dc5150486ee61bfeb1cb70
Yenifer Ramírez <yeniferramirez11@gmail.com>
2022-02-16 10:18:29 -0400

    Primeros Cambios

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar 
+    color="#3853D8" 
+    dark
+  >
+    <v-btn icon>
+      <v-icon> mdi-magnify </v-icon>
+    </v-btn>
+    <v-divider vertical class="ml-1 mr-3"></v-divider>
+    <v-btn icon>
+      <v-icon> mdi-menu </v-icon>
+    </v-btn>
+    <v-toolbar-title>
+      <span class="caption">Seleccionar</span><br>Categoría
+    </v-toolbar-title>
+    <v-vertical class="ml-5"></v-vertical>
+    <div class="title-nav">
+
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.title-nav {
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,31 @@
 <template>
-  <hello-world />
+<v-app>
+  <NavBar class="index" />
+  <Dashboard />
+  <Footer />
+</v-app>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  import NavBar from '../components/NavBar.vue'
+  import Dashboard from '../components/Dashboard.vue'
+  import Footer from '../components/Footer.vue'
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
+        NavBar,
+        Dashboard,
+        Footer
     },
   }
 </script>
+
+<style scoped>
+
+.index {
+  z-index: 9999;
+}
+
+</style>
